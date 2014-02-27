9b28f2f87b264d27215ee1e185353608a6842bb7
Shu Kong <aimerykong@gmail.com>
2014-02-27 13:49:26 +0800

    Update genRFcandidates.m

--- genRFcandidates.m
+++ genRFcandidates.m
@@ -1,4 +1,4 @@
-function [RFlist imgCenter] = genRFcandidates( imgSize, PartitionFactor, LeapStepFactor )
+function [RFlist imgCenter] = genRFcandidates( type, LeapStepFactor, imgSize, PartitionFactor )
 %   Given an image and a decisive factor, the function generates a set of
 %   recpetive field candidates, and records the partition of these
 %   regions/fields in a string.
@@ -21,56 +21,83 @@ function [RFlist imgCenter] = genRFcandidates( imgSize, PartitionFactor, LeapSte
 %   Nov. 2013
 
 %% default parameters to create a receptive field (RF) model 
-if nargin < 3
-    LeapStepFactor = 1;
-end
-if nargin < 2
+if nargin < 4
     PartitionFactor = 4;
 end
-if nargin < 1
+if nargin < 3    
     imgSize = [100 100];
 end
+if nargin < 2    
+    LeapStepFactor = 0.5;
+end
+if nargin < 1
+    type = 'pyramid'; % 'grid'; %
+end
+
 imgSize = imgSize(1:2);
 imgCenter = round(imgSize/2);
 
 %%
-% what is changing? only region size!!
-% leapSize is fixed forever!!
-
-%%
 gridSize = floor(imgSize / PartitionFactor);
-
 RFlist = [];
 
-binSize = PartitionFactor; % small grid size
-LeapStep = floor( gridSize*LeapStepFactor );
-
-for ii = 1:binSize
-    for jj = 1: binSize
-        regionSize = gridSize + ([ii jj]-1).*gridSize;
-        
-        for vertical = 1:PartitionFactor/LeapStepFactor-ii+1
-            anchor(1) = 1 + LeapStep(1)*(vertical-1);
-            if anchor(1) > imgSize(1)
-                continue;
+if strcmp(type,'pyramid')    
+    binSize = PartitionFactor; % small grid size
+    LeapStep = floor( gridSize*LeapStepFactor );
+    for ii = 1:binSize
+        for jj = 1: binSize
+            regionSize = gridSize + ([ii jj]-1).*gridSize;
+            
+            for vertical = 1:PartitionFactor/LeapStepFactor-ii+1
+                anchor(1) = 1 + LeapStep(1)*(vertical-1);
+                if anchor(1) > imgSize(1)
+                    continue;
+                end
+                for horizontal = 1:PartitionFactor/LeapStepFactor-jj+1
+                    anchor(2) = 1 + LeapStep(2)*(horizontal-1);
+                    
+                    posBin = zeros(4,1);
+                    posBin(1:2) = anchor(:);
+                    posBin(3:4) = anchor(:) + regionSize(:)-1;
+                    posBin(5:6) = round( (posBin(1:2)+posBin(3:4)) /2 );
+                    posBin(7) = norm(abs(posBin(5:6) - imgCenter(:)), 'fro');
+                    if posBin(3) <= imgSize(1) && posBin(4) <= imgSize(2)
+                        RFlist = [RFlist, posBin];
+                    end
+                    
+                end
             end
-            for horizontal = 1:PartitionFactor/LeapStepFactor-jj+1
-                anchor(2) = 1 + LeapStep(2)*(horizontal-1);
-                
-                posBin = zeros(4,1);
-                posBin(1:2) = anchor(:);
-                posBin(3:4) = anchor(:) + regionSize(:)-1;
-                posBin(5:6) = round( (posBin(1:2)+posBin(3:4)) /2 );
-                posBin(7) = norm(abs(posBin(5:6) - imgCenter(:)), 'fro');
-                if posBin(3) <= imgSize(1) && posBin(4) <= imgSize(2)
-                    RFlist = [RFlist, posBin];
-                end                
-                
-            end
-        end        
+        end
     end
+elseif strcmp(type,'grid')
+    binSize = floor(PartitionFactor/2); % small grid size
+    LeapStep = floor( gridSize*LeapStepFactor );
+    for ii = 1:binSize
+        for jj = 1: binSize
+            regionSize = gridSize + ([ii jj]-1).*gridSize;
+            
+            for vertical = 1:PartitionFactor/LeapStepFactor-ii+1
+                anchor(1) = 1 + LeapStep(1)*(vertical-1);
+                if anchor(1) > imgSize(1)
+                    continue;
+                end
+                for horizontal = 1:PartitionFactor/LeapStepFactor-jj+1
+                    anchor(2) = 1 + LeapStep(2)*(horizontal-1);
+                    
+                    posBin = zeros(4,1);
+                    posBin(1:2) = anchor(:); % upper-left corner
+                    posBin(3:4) = anchor(:) + regionSize(:)-1; % bottom-right corner
+                    posBin(5:6) = round( (posBin(1:2)+posBin(3:4)) /2 ); % RF center
+                    posBin(7) = norm(abs(posBin(5:6) - imgCenter(:)), 'fro'); % distance to image center
+                    if posBin(3) <= imgSize(1) && posBin(4) <= imgSize(2)
+                        RFlist = [RFlist, posBin];
+                    end                    
+                end
+            end
+        end
+    end
+else
 end
-
 
 
 
